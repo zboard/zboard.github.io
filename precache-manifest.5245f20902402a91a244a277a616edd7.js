e00aedafb70eec7d48df57fa84a0e947f4d5365e
Agarast <agarast@gmail.com>
2020-10-14 23:01:02 +0200

    update date & cache busting

---
+++ precache-manifest.5245f20902402a91a244a277a616edd7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b77071cdc41861ec909f05149e96686",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6cd0d497fb1c0e04039a",
+    "url": "./static/css/main.25d300a5.chunk.css"
+  },
+  {
+    "revision": "1d177a3941ceeff24c52",
+    "url": "./static/js/2.20091067.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.20091067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cd0d497fb1c0e04039a",
+    "url": "./static/js/main.e74e174d.chunk.js"
+  },
+  {
+    "revision": "db6007875f562d35c1df",
+    "url": "./static/js/runtime-main.6294823c.js"
+  },
+  {
+    "revision": "6f2bfd2dd20a56fa1bc39da4d8c615fe",
+    "url": "./static/media/Jura-Bold.6f2bfd2d.ttf"
+  },
+  {
+    "revision": "ef9b2eff47c84477a41de530e3131e25",
+    "url": "./static/media/Jura-Medium.ef9b2eff.ttf"
+  },
+  {
+    "revision": "ebbbf014fc0411aedf149c2c6d6f413d",
+    "url": "./static/media/viewer.ebbbf014.svg"
+  }
+]);
